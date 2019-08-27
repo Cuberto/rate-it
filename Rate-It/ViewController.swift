fd6a56d9e3f70ca1454f2c1451498a35a5587192
Anton Skopin <askopin@gmail.com>
2019-08-27 10:19:42 +0300

    Initial Commit

---
+++ Rate-It/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rate-It
+//
+//  Created by Anton Skopin on 27.08.2019.
+//  Copyright © 2019 cuberto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
